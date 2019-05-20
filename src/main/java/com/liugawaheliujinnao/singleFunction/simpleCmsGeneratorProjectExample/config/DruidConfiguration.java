228a090b71095f56029998809a8f1e9018093e47
liugawaheliujinnao <liugawaheliujinnao@126.com>
2019-05-21 00:28:20 +0800

    update plugin

--- src/main/java/com/liugawaheliujinnao/singleFunction/simpleCmsGeneratorProjectExample/config/DruidConfiguration.java
+++ src/main/java/com/liugawaheliujinnao/singleFunction/simpleCmsGeneratorProjectExample/config/DruidConfiguration.java
@@ -100,8 +100,8 @@ public class DruidConfiguration {
         servletRegistrationBean.setServlet(new StatViewServlet());
         servletRegistrationBean.addUrlMappings("/druid/*");
         Map<String, String> initParameters = new HashMap<String, String>();
-        initParameters.put("loginUsername", "root"); // 用户名
-        initParameters.put("loginPassword", "19900624ova"); // 密码
+        initParameters.put("loginUsername", "xxx"); // 用户名
+        initParameters.put("loginPassword", "xxxxxxxxxx"); // 密码
         initParameters.put("resetEnable", "false"); // 禁用HTML页面上的“Reset All”功能
         initParameters.put("allow", ""); // IP白名单 (没有配置或者为空，则允许所有访问)
         servletRegistrationBean.setInitParameters(initParameters);
